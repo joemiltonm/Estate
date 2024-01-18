cabb70a7a5daa5fb069e04e934e2e801dbc49300
joemiltonm <m.joemilton@gmail.com>
2024-01-18 11:07:34 +0530

    Sign up page completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home' 
 import About from './pages/About'
 import Signin from './pages/Signin'
-import Signout from './pages/Signout'
+import Signup from './pages/Signup'
 import Profile from './pages/Profile'
 import Header from './components/Header'
 
@@ -15,7 +15,7 @@ export default function App() {
         <Route path="/" element = {<Home/>}/>
         <Route path="/About" element = {<About/>}/>
         <Route path="/Sign-in" element = {<Signin/>}/>
-        <Route path="/Sign-out" element = {<Signout/>}/>
+        <Route path="/Sign-up" element = {<Signup/>}/>
         <Route path="/Profile" element = {<Profile/>}/>
       </Routes>
     </BrowserRouter>
