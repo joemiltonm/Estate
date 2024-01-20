9880c52f318b60ec48bee993d2a2aaf26f82ee08
joemiltonm <m.joemilton@gmail.com>
2024-01-20 11:19:05 +0530

    Header updated for sign in

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Signin from './pages/Signin'
 import Signup from './pages/Signup'
 import Profile from './pages/Profile'
 import Header from './components/Header'
+import PrivateRoute from './components/PrivateRoute'
 
 export default function App() {
   
@@ -16,7 +17,9 @@ export default function App() {
         <Route path="/About" element = {<About/>}/>
         <Route path="/Sign-in" element = {<Signin/>}/>
         <Route path="/Sign-up" element = {<Signup/>}/>
-        <Route path="/Profile" element = {<Profile/>}/>
+        <Route element = {<PrivateRoute/>} >
+          <Route path="/Profile" element = {<Profile/>}/>
+        </Route>
       </Routes>
     </BrowserRouter>
   )
